1417972630582d902f4f2f284727f8efe0bb355c
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-07-09 16:07:25 -0300

    feat: refinements in the home

--- src/screens/Home/View.tsx
+++ src/screens/Home/View.tsx
@@ -1,34 +1,57 @@
-import React from 'react';
-import {FlatList} from 'react-native-gesture-handler';
-import Error from '../../assets/svgs/error.svg';
+import React, {useCallback, useState} from 'react';
+import {Keyboard} from 'react-native';
+import {FlatList, TouchableWithoutFeedback} from 'react-native-gesture-handler';
 import {CoinListItem} from '../../components/CoinListItem';
 import {PriceVariation} from '../../components/PriceVariation';
-import {Container, Title} from './styles';
+import {SearchBar} from '../../components/SearchBar';
+import {Container} from './styles';
 import {HomeViewProps} from './types';
 
-export const View = ({data, hasError}: HomeViewProps) => {
-  if (hasError) {
-    <Container>
-      <Error height={100} width={100} />
-    </Container>;
-  }
+export const View = ({data}: HomeViewProps) => {
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearch = (text: string) => {
+    const filtered = data?.filter(
+      item =>
+        item.name.toLowerCase().includes(text.toLowerCase()) ||
+        item.symbol.toLowerCase().includes(text.toLowerCase()),
+    );
+
+    setFilteredData(filtered);
+  };
+
+  const onHandleKeyboardDismiss = useCallback(() => {
+    Keyboard.dismiss();
+  }, []);
 
   return (
-    <FlatList
-      data={data}
-      keyExtractor={item => item.id}
-      contentContainerStyle={{paddingHorizontal: 24, backgroundColor: '#fff'}}
-      ListHeaderComponent={<Title>Crypto</Title>}
-      ListHeaderComponentStyle={{alignItems: 'center'}}
-      renderItem={({item}) => (
-        <CoinListItem
-          logo={item.image}
-          name={item.name}
-          ticker={item.symbol}
-          price={item.current_price}>
-          <PriceVariation variation={item.price_change_percentage_24h} />
-        </CoinListItem>
-      )}
-    />
+    <TouchableWithoutFeedback
+      onPress={onHandleKeyboardDismiss}
+      style={{backgroundColor: 'red'}}>
+      <Container>
+        <SearchBar placeholder="Search crypto" onChangeText={handleSearch} />
+      </Container>
+      <FlatList
+        data={filteredData}
+        keyExtractor={item => item.id}
+        style={{backgroundColor: 'red'}}
+        // ListHeaderComponent={<Title>Crypto</Title>}
+        // ListHeaderComponentStyle={{alignItems: 'center'}}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          backgroundColor: '#fff',
+          height: '100%',
+        }}
+        renderItem={({item}) => (
+          <CoinListItem
+            logo={item.image}
+            name={item.name}
+            ticker={item.symbol}
+            price={item.current_price}>
+            <PriceVariation variation={item.price_change_percentage_24h} />
+          </CoinListItem>
+        )}
+      />
+    </TouchableWithoutFeedback>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,12 @@
 import {styled} from 'styled-components/native';
 
+export const Content = styled.View`
+  background-color: red;
+`;
+
 export const Container = styled.View`
-  flex: 1;
   background-color: #ffffff;
-  align-items: center;
-  justify-content: center;
+  padding: 24px;
 `;
 
 export const Title = styled.Text`
