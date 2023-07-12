d984074362023edc01ea1ef63a1fe9c9a8ed1212
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-07-12 17:32:14 -0300

    feat: refinements in the hook

--- src/hooks/useGetMarketCoins.tsx
+++ src/hooks/useGetMarketCoins.tsx
@@ -5,21 +5,26 @@ import {api} from '../services/api';
 export const useGetMarketCoins = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<Array<DataType> | null>(null);
-  const [page, setPage] = useState<number>(1);
   const [hasError, setHasError] = useState(false);
 
-  const url =
-    '/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
+  const url = '/api/v3/coins/markets';
 
   async function fetchMarketCoinsList() {
     try {
       setHasError(false);
+      setLoading(true);
       const response = await api.get(url, {
-        params: {page: page},
+        params: {
+          page: 1,
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          sparkline: false,
+          locale: 'en',
+        },
       });
       setData(response.data);
     } catch (error) {
-      console.log(error);
       setLoading(false);
       setHasError(true);
     } finally {
@@ -32,7 +37,5 @@ export const useGetMarketCoins = () => {
     loading,
     fetchMarketCoinsList,
     hasError,
-    setPage,
-    page,
   };
 };
